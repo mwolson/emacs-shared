8223991a4ec82079563fc0fa7f2e7c2153b2c2f5
Michael Olson <mwolson@member.fsf.org>
2016-11-26 16:17:07 -0800

    install-packages: Allow packages to initialize so we can use the package-utils library

--- install-packages.el
+++ install-packages.el
@@ -1,7 +1,7 @@
 (load-file (concat default-directory "init/settings.el"))
 
 (require 'package)
-(package-initialize t)
+(package-initialize)
 (package-refresh-contents)
 
 (mapc #'package-install package-selected-packages)
