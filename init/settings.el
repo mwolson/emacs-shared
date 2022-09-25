f2cf40c2274c91fc581990a58b1490d2539d0d94
Mike Olson <mwolson@member.fsf.org>
2022-09-24 22:01:43 -0400

    Dired: Kill prev buffer when opening new buffer

--- init/settings.el
+++ init/settings.el
@@ -30,6 +30,7 @@
  '(delete-selection-mode t)
  '(diff-switches "-u")
  '(dired-dwim-target t)
+ '(dired-kill-when-opening-new-dired-buffer t)
  '(dired-recursive-copies 'always)
  '(dired-recursive-deletes 'always)
  '(doc-view-resolution 132)
