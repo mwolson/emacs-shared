4347c71dd497970e3575b17986f1311def026a21
Mike Olson <mwolson@member.fsf.org>
2023-08-12 15:18:55 -0400

    Turn off `C-x C-f` binding for helm and go back to counsel

--- init/shared-init.el
+++ init/shared-init.el
@@ -970,7 +970,7 @@ With \\[universal-argument], also prompt for extra rg arguments and set into RG-
      (define-key helm-find-files-map "/" #'helm-execute-persistent-action)
      (define-key helm-find-files-map (kbd "TAB") #'helm-execute-persistent-action)))
 
-(global-set-key "\C-x\C-f" 'helm-find-files)
+;(global-set-key "\C-x\C-f" 'helm-find-files)
 
 (defun my-preload-helm ()
   (require 'helm-files))
