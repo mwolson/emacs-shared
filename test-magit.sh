6d28e0cc587bf4bb60c18ec66a66e37f9a9bc0d2
Mike Olson <mwolson@member.fsf.org>
2024-09-06 15:10:08 -0400

    Remove magit submodule

--- test-magit.sh
+++
@@ -1,6 +0,0 @@
-
-# Have to base64-encode this, since sh on windows treats ':' character as some kind of escape sequence
-ELISP_DIR=$(emacs --batch -q --no-site-file --eval "(message (base64-encode-string (expand-file-name \"elisp\")))" 2>&1)
-
-cd elisp/magit
-make test EFLAGS="--eval \"(add-to-list 'load-path (base64-decode-string \\\"$ELISP_DIR\\\") t)\""
